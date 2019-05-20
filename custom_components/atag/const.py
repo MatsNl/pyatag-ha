58bee2870de243c87384524f3b2e475c0e0b8173
Nelissen <Mats.Nelissen@ing.com>
2019-05-20 17:05:59 +0200

    .

--- custom_components/atag/const.py
+++ custom_components/atag/const.py
@@ -2,10 +2,9 @@
 """Constants for Homeassistant"""
 
 DOMAIN = 'atag'
-ATAG_HANDLE = 'atag_handle'
-DATA_LISTENER = 'listener'
+ATAG_HANDLE = 'atag_data'
+DATA_LISTENER = 'atag_listener'
 SIGNAL_UPDATE_ATAG = 'atag_update'
 CONF_INTERFACE = 'interface'
 DEFAULT_INTERFACE = 'eth0'
-
 DEFAULT_PORT = 10000
