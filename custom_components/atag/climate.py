8c68e3ad71e2f14a8fe13448f77b1f1924c1b9fa
Nelissen <Mats.Nelissen@ing.com>
2019-05-20 22:07:22 +0200

    .

--- custom_components/atag/climate.py
+++ custom_components/atag/climate.py
@@ -1,25 +1,23 @@
 """Initialization of ATAG One climate platform."""
 import logging
+from homeassistant.components.climate import (ClimateDevice)
 from homeassistant.components.climate.const import (
-    STATE_AUTO, STATE_MANUAL, SUPPORT_TARGET_TEMPERATURE, SUPPORT_OPERATION_MODE)
-#    , SUPPORT_TARGET_HUMIDITY_LOW,
-#    SUPPORT_TARGET_HUMIDITY_HIGH)
-from homeassistant.core import callback
-from homeassistant.helpers.dispatcher import async_dispatcher_connect
-
+    STATE_HEAT,
+    STATE_AUTO, STATE_MANUAL, SUPPORT_TARGET_TEMPERATURE, SUPPORT_OPERATION_MODE,
+    SUPPORT_TARGET_HUMIDITY_LOW, SUPPORT_TARGET_HUMIDITY_HIGH, ATTR_CURRENT_TEMPERATURE,
+    ATTR_OPERATION_MODE, DEFAULT_MAX_TEMP, DEFAULT_MIN_TEMP, SUPPORT_ON_OFF)
 from homeassistant.const import (TEMP_CELSIUS, ATTR_TEMPERATURE)
 
-from homeassistant.components.climate import (ClimateDevice)
-from homeassistant.components.climate.const import (ATTR_CURRENT_TEMPERATURE, ATTR_OPERATION_MODE,
-                                                    DEFAULT_MAX_TEMP, DEFAULT_MIN_TEMP)
-
+from homeassistant.core import callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.util.temperature import convert as convert_temperature
+
 from .const import (ATAG_HANDLE, DOMAIN, SIGNAL_UPDATE_ATAG)
 
-SUPPORT_FLAGS = (SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE)  # |
-#                 SUPPORT_TARGET_HUMIDITY_LOW | SUPPORT_TARGET_HUMIDITY_HIGH)
+SUPPORT_FLAGS = (SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE |
+                 SUPPORT_TARGET_HUMIDITY_LOW | SUPPORT_TARGET_HUMIDITY_HIGH | SUPPORT_ON_OFF)
 
-OPERATION_LIST = [STATE_MANUAL, STATE_AUTO]
+OPERATION_LIST = [STATE_HEAT, STATE_AUTO]
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -38,7 +36,9 @@ class AtagOneThermostat(ClimateDevice):  # pylint: disable=abstract-method
         _LOGGER.debug("Initializing Atag climate platform")
         self.atag = hass.data[DOMAIN][ATAG_HANDLE]
         self._name = 'Atag thermostat'
-        #self._operation_list = [STATE_MANUAL, STATE_AUTO]
+        self._humidity = None
+        self._on = True
+        self._operation_list = OPERATION_LIST
         _LOGGER.debug("Atag climate initialized")
 
     async def async_added_to_hass(self):
@@ -104,9 +104,14 @@ class AtagOneThermostat(ClimateDevice):  # pylint: disable=abstract-method
 
     @property
     def min_temp(self):
-        """Return the maximum temperature."""
+        """Return the minimum temperature."""
         return convert_temperature(DEFAULT_MIN_TEMP, TEMP_CELSIUS,
                                    self.temperature_unit)
+
+    @property
+    def is_on(self):
+        """Return true if the device is on."""
+        return self._on
 
     async def async_set_temperature(self, **kwargs):
         """Set new target temperature."""
